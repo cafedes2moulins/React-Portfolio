72dc7d19ead1d66f01c4ffc968216b4eebfb49bc
unknown <tara.alsaidi@gmail.com>
2023-10-26 18:55:54 -0700

    Initialized React file structure

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import Header from './components/header/Header'
+import Nav from './components/nav/Nav'
+import About from './components/about/About'
+import Projects from './components/projects/Projects'
+import Contact from './components/contact/Contact'
+import Footer from './components/footer/Footer'
+
+const App = () => {
+    return (
+        <>
+            <Header />
+            <Nav />
+            <About />
+            <Projects />
+            <Contact />
+            <Footer />
+        </>
+    )
+}
+
+export default App
