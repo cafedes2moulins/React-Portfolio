ceabab9989f19d20f118f7b9e8267dfbb82b701f
Tara Alsaidi <talsaidi@ucdavis.edu>
2023-10-31 13:45:04 -0700

    Added header components and styling

---
+++ src/components/header/ActionBar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const ActionBar = () => {
+    return (
+        <div className='contactbar'>
+            <a href='#contact' className='btn btn-primary'>Let's Chat</a>
+
+        </div>
+    )
+}
+
+export default ActionBar
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,5 +1,8 @@
 import React from 'react'
 import './header.css'
+import ActionBar from './ActionBar'
+import Socials from './Socials'
+import Flowers from '../../assets/flowers.jpg'
 
 const Header = () => {
     return (
@@ -8,6 +11,13 @@ const Header = () => {
                 <h5>Hello, I'm</h5>
                 <h1>Tara</h1>
                 <h5 className="text-light">Fullstack Developer</h5>
+                <ActionBar />
+                <Socials />
+
+                <div className="portrait">
+                    <img src={Flowers} alt='by <a href="https://unsplash.com/@ninjason?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Jason Leung</a> on <a href="https://unsplash.com/photos/a-bunch-of-flowers-that-are-sitting-on-a-table-NsaQk3nQ77g?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Unsplash</a>
+  '/>
+                </div>
             </div>
         </header>
     )
